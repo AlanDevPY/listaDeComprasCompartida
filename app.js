3cd2d749e5a7a88058dbff99c16c88498b880043
Emanuromet <emanuromet@gmail.com>
2023-09-01 20:18:24 -0400

    add code

---
+++ app.js
@@ -0,0 +1,23 @@
+import { agregarProducto } from "./firebase.js"
+
+// Obtenemos el boton del formulario para cargar los datos
+let btnGuardar = document.getElementById('btnGuardar')
+
+// creamos una funcion que nos permita guardar los datos en la base de datos de firestore
+btnGuardar.addEventListener('click', () => {
+    let persona = document.getElementById('selectorName').value;
+    let producto = document.getElementById('inputProducto').value;
+    let cantidad = document.getElementById('inputCantidad').value;
+
+    agregarProducto(persona, producto, cantidad) // agregamos los datos a la base de datos de firestore
+    
+    // con esto reseteamos el form en blanco
+    document.getElementById('selectorName').value = '';
+    document.getElementById('inputProducto').value = '';
+    document.getElementById('inputCantidad').value = '';
+})
+
+// Creamos la funcion para mostrat los productos agregados
+
+
+
---
+++ firebase.js
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFKON90lj8HWLiS_pOnb0k9wZj3EeyIec",
+  authDomain: "listadecomprascompartida-c215d.firebaseapp.com",
+  projectId: "listadecomprascompartida-c215d",
+  storageBucket: "listadecomprascompartida-c215d.appspot.com",
+  messagingSenderId: "615407700979",
+  appId: "1:615407700979:web:9c93ccd297138bd7c2e086"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore()
+
+// Creando Funciones
+
+// funcion de agregar productos a la base de datos de Firestore
+export const agregarProducto = (producto, cantidad, persona) => {
+  try {
+    const options = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: false, // Usar formato de 24 horas
+    };
+
+    const time = new Date().toLocaleString("es-ES", options); // Cambia 'es-ES' al código de tu localización
+    addDoc(collection(db,"listaDeCompras"),{
+      producto: producto,
+      cantidad: cantidad,
+      persona: persona,
+      time:time
+    });
+  }
+  catch {
+    console.error('Error al agregar producto', error)
+  }
+}
+
+
